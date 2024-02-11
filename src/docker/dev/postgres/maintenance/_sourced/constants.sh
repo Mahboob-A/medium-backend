3b8f08715391a85e41cdc7de13b3dce77da14e86
root <root@ip-172-31-15-44.ap-south-1.compute.internal>
2024-02-11 17:26:16 +0000

    fix: fix bugs related to postgres backup and restore scripts

--- src/docker/dev/postgres/maintenance/_sourced/constants.sh
+++ src/docker/dev/postgres/maintenance/_sourced/constants.sh
@@ -1,6 +1,6 @@
-!#/usr/bin/env bash 
+#!/usr/bin/env bash 
 
 
-BACKUP_DIR_PATH ='/backups'
+BACKUP_DIR_PATH='/backups'
 
 BACKUP_FILE_PREFIX='backup'
--- src/docker/dev/postgres/maintenance/_sourced/messages.sh
+++ src/docker/dev/postgres/maintenance/_sourced/messages.sh
@@ -1,4 +1,4 @@
-!#/usr/bin/env bash 
+#!/usr/bin/env bash 
 
 # prints new line 
 message_newline(){
