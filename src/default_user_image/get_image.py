d4a4cd6b21e450848974ac1238d3cd8fda8b841c
Mahboob-A <iammahboob.a@gmail.com>
2024-02-13 14:34:42 +0000

    add: custom user model and custom user manager

---
+++ src/default_user_image/get_image.py
@@ -0,0 +1,18 @@
+
+import requests 
+import json 
+URL = "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg"
+
+response = requests.get(URL)
+
+if response.status_code == 200: 
+    with open('user-image.jpg', 'wb') as f: 
+        f.write(response.content)
+        print('image downloaded successfully!')
+
+else:
+    print(f'ERROR: {response.status_code}')
+    print(response.text)
+
+
+
