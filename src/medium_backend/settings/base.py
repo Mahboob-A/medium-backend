541fc210bcf0a55bf88e2e2166058beb42b95278
Mahboob-A <iammahboob.a@gmail.com>
2024-03-05 20:57:13 +0530

    add: dj_auth, allauth, drf settings

--- src/medium_backend/settings/base.py
+++ src/medium_backend/settings/base.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 ############################ Path and Env 
 
 from pathlib import Path
+from datetime import timedelta
 
 import environ
 env = environ.Env()
@@ -235,6 +236,58 @@ if USE_TZ:
 CORS_URLS_REGEX = r"^api/.*$"
 
 AUTH_USER_MODEL = "users.CustomUser"
+
+''' API and AUTH SETTINGS '''
+
+#   Rest Framework Settings 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication", 
+    ], 
+    "DEFAULT_PERMISSION_CLASSES" : [
+        "rest_framework.permissions.IsAuthenticated", 
+    ], 
+    "DEFAULT_FILTER_BACKENDS" : [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ]
+}
+
+# DRF Simple JWT Settings 
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES" : ("Bearer", ), 
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=25), 
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1), 
+    "ROTATE_REFRESH_TOKENS": True, 
+    "SIGNING_KEY": env("SIGNING_KEY"), 
+    "USER_ID_FIELD": "id", 
+    "USER_IF_CLAIM": "user_id", 
+}
+
+# DJ Rest AUTH Settings 
+REST_AUTH = {
+    "USE_JWT": True, 
+    "JWT_AUTH_COOKIE": "medium-backend-access-token", 
+    "JWT_AUTH_REFRESH_COOKIE": "medium-backend-refresh-token", 
+    "REGISTER_SERIALIZER": "core_apps.users.serializers.CustomRegistrationSerializer", 
+}
+
+# Auth Backend 
+AUTHENTICATION_BACKENDS = [
+    "allauth.account.auth_backends.AuthenticationBackend", 
+    "django.contrib.auth.backends.ModelBackend", 
+]
+
+# Other allauth Auth Settings 
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True 
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None 
+ACCOUNT_USERNAME_REQUIRED = False 
+
+########################################################
+
 
 LOGGING = {
     "version": 1, 
