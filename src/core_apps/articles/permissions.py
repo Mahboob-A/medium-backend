347d06cabb1cba018d0043a3a66aa865a36ccb2f
Mahboob-A <iammahboob.a@gmail.com>
2024-03-09 22:29:43 +0530

    Add: Permission Class for core_apps.article.Article model objects
    Add IsOwnerOrReadOnly to control non-owner/viewer users only get SAFE_METHODS access over the Article objects and authentic Authors get the correct UNSAFE_METHODS on Article objects like Put, Patch, Post, Delete etc.

---
+++ src/core_apps/articles/permissions.py
@@ -0,0 +1,15 @@
+
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+class IsOwnerOrReadOnly(BasePermission): 
+        ''' Permission Class to Allow Un-safe Methods in Article objects Only to the Respective Author/User'''
+        def has_object_permission(self, request, view, obj):
+                
+                # SAFE_METHODS ('GET', 'HEAD', 'OPTIONS')
+                if request.method in SAFE_METHODS:  
+                        return True 
+                
+                # if request not in SAFE_METHODS only grant if the object(article) is associated with author/user (as Article has relation with User with author key)
+                # and request.user is also author/same user  
+                return bool(obj.author==request.user)
+                
