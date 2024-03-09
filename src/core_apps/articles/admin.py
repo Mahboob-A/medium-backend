ed8134c8bf57b64417388869df7edc09486e5ff0
Mahboob-A <iammahboob.a@gmail.com>
2024-03-09 21:51:53 +0530

    Add: Register core_apps.articles Model in admin and core_apps.
    Registered Article and ArticleView class in admin with comprehenisve arguments

--- src/core_apps/articles/admin.py
+++ src/core_apps/articles/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article, ArticleViews
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin): 
+        list_display = ['pkid', 'id', 'author', 'title', 'get_view_count', 'tags']
+        list_display_links = ['pkid', 'id', 'author', 'title']
+        list_filter =  ['created_at', 'updated_at']
+        search_fields = ['title', 'author', 'tags']
+        ordering = ['-created_at']
+        
+        readonly_fields = ['view_count']
+        date_hierarchy = 'created_at'
+        fieldsets = [
+                {'legend' : 'Article Information', 'fields' : ['title', 'slug', 'author']}, 
+                {'legend' : 'Content', 'fields' : ['description', 'body', 'tags']}, 
+                {'legend' : 'Analytics', 'fields' : ['view_count']}
+        ]
+        
+        def get_view_count(self, obj): 
+                return self.obj.view_count()
+        
+@admin.register(ArticleViews)
+class ArticleAdmin(admin.ModelAdmin): 
+        list_display = ['pkid', 'id', 'article', 'user', 'viewer_ip']
+        list_display_links = ['pkid', 'id', 'article', 'user']
+        list_filter =  ['created_at', 'updated_at']
+        search_fields = ['article', 'user', 'viewer_ip']
+        ordering = ['-created_at']
