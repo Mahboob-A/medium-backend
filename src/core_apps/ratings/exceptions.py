f5b1b817a64840764c5c6c2080c89cb832f7c566
Mahboob-A <iammahboob.a@gmail.com>
2024-03-13 14:29:27 +0530

    Add: core_apps.ratings.exceptions module to handle custom exceptions
    Add YouHaveAlreadyRatedException to handle when user has already rated an article.

---
+++ src/core_apps/ratings/exceptions.py
@@ -0,0 +1,13 @@
+
+from django.utils.translation import gettext_lazy as _ 
+
+from rest_framework.exceptions import APIException
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+
+class YouHaveAlreadyRatedException(APIException): 
+        status_code = HTTP_400_BAD_REQUEST
+        default_detail = _('You have already rated this article')
+        default_code = 'bad_request'
+        
+        
