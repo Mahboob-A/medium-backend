e32b4c078d3961bf578e8f888d82f43647bf42f9
Mahboob-A <iammahboob.a@gmail.com>
2024-03-14 21:40:29 +0530

    Add: core_apps.bookmarks.model to store bookmarks or articles Add Bookmark model for an article is bookmarked by an user

--- src/core_apps/bookmarks/models.py
+++ src/core_apps/bookmarks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+from core_apps.common.models import TimeStampModel
+from core_apps.articles.models import Article
+
+
+User = get_user_model()
+
+
+class Bookmark(TimeStampModel): 
+        ''' Model for Bookmarks of an Article by an User '''
+        user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookmarks')
+        article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='bookmarks')
+        
+
+        class Meta: 
+                unique_together = ['user', 'article']
+                ordering = ['-created_at']
+
+        def __str__(self) -> str:
+                return f'{self.user.first_name.title()} {self.user.last_name.title()} bookmarked {self.article.title} !'
+        
