30f49adbefd613742c0ed81e1aace7d0a3dd0026
Mahboob-A <iammahboob.a@gmail.com>
2024-03-14 22:18:53 +0530

    Add core_apps.bookmarks.serializers and admin for Bookmarks model > Add BookmarkSerializer and BookmarkAdmin for Bookmark model. >

---
+++ src/core_apps/bookmarks/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Bookmark
+
+
+class BookmarkSerializer(serializers.ModelSerializer): 
+        article_title = serializers.CharField(source='article.title', read_only=True)
+        user_first_name = serializers.CharField(source='user.first_name', read_only=True)
+        user_last_name = serializers.CharField(source='user.last_name', read_only=True)
+        
+        class Meta: 
+                model = Bookmark
+                fields = ['id', 'article_title', 'user_first_name', 'user_last_name', 'created_at']
+                read_only_fields = ['user']
