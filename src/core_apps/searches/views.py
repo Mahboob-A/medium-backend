ca04b0a94223d36a3a7dd90de5c38e5b00695923
Mahboob-A <iammahboob.a@gmail.com>
2024-03-18 11:42:18 +0530

    Add: core_apps.searches.views.ArticleElasticSearchView API for searching Article models with elasticsearch

--- src/core_apps/searches/views.py
+++ src/core_apps/searches/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django_elasticsearch_dsl_drf.filter_backends import FilteringFilterBackend, IdsFilterBackend, OrderingFilterBackend, DefaultOrderingFilterBackend, SearchFilterBackend
+
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+
+from rest_framework.permissions import AllowAny
+
+from .documents import ArticleDocument
+from .serializers import ArticleElasticSearchSerializer
+
+
+
+
+class ArticleElasticSearchView(DocumentViewSet): 
+        ''' API for enabling searching with Elasticsearch of Article Model '''
+        document = ArticleDocument
+        serializer_class = ArticleElasticSearchSerializer
+        lookup_field = 'id' # for database lookup 
+        permission_classes = [AllowAny]
+        
+        filter_backends = [
+                FilteringFilterBackend, IdsFilterBackend, OrderingFilterBackend, DefaultOrderingFilterBackend, SearchFilterBackend
+        ]
+         
+        # search by below fields 
+        search_fields = ('title', 'description', 'body', 'author_first_name', 'author_last_name', 'tags')
+        
+        
+        filter_fields = {
+                'slug' : 'slug.raw', 
+                'tags' : 'tags', 
+                'created_at' : 'created_at'
+        }
+        
+        ordering_fields = {'created_at' : 'created_at'}
+        
+        ordering = ('-created_at')
+        
+        
+        
