5b35cfa0f3319eba4819fc8caba56ec0aa412d12
Mahboob-A <iammahboob.a@gmail.com>
2024-03-18 17:00:46 +0530

    Test: Add: src.conftest and core_appsl.users.tests
    Setup UserFactory and conftest for using pytest fixxtures for tesing User model

---
+++ src/core_apps/users/tests/factories.py
@@ -0,0 +1,34 @@
+import factory 
+from faker import Factory as FakerFactory 
+
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+
+
+faker = FakerFactory().create()
+
+User = get_user_model()
+
+
+@factory.django.mute_signal(post_save)
+class UserFactory(factory.django.DjangoModelFactory): 
+
+    class Meta:
+        model = User
+
+    first_name = factory.LazyAttribute(lambda x: faker.first_name)
+    last_name = factory.LazyAttribute(lambda x: faker.last_name)                
+    email = factory.LazyAttribute(lambda x: faker.email)
+    password = factory.LazyAttribute(lambda x: faker.password)
+    is_active = True 
+    is_staff = False 
+
+    # def _create_user(self, first_name, last_name, email, password, **extra_fields):
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):   
+        manager = cls._get_manager(model_class)
+        if "is_superuser" in kwargs:
+            return manager.create_superuser(*args, **kwargs)
+        else:
+            return manager.create_user(*args, **kwargs)
+
